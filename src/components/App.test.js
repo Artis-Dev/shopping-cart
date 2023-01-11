97f62f6b26e762098add5ee94097c6d9e32d32a3
ArtisDev <artisdev@proton.me>
2023-01-11 13:28:37 +0200

    Update project structure

---
+++ src/components/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Hello world! string', () => {
+  render(<App />);
+  const stringElement = screen.getByText(/Hello world!/i);
+  expect(stringElement).toBeInTheDocument();
+});
