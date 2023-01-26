684326b2e9ab2683ba12cef2c140095b646df1eb
ArtisDev <artisdev@proton.me>
2023-01-26 17:26:46 +0200

    Add dummy products for placeholders

---
+++ src/utils/products.js
@@ -0,0 +1,12 @@
+import mickey from '../assets/mickey.webp';
+
+const products = [
+  { id: '0', name: 'Product Name #1', price: 10.0, image: mickey },
+  { id: '1', name: 'Product Name #2', price: 14.99, image: mickey },
+  { id: '2', name: 'Product Name #3', price: 9.5, image: mickey },
+  { id: '3', name: 'Product Name #4', price: 7.7, image: mickey },
+  { id: '4', name: 'Product Name #5', price: 12.0, image: mickey },
+  // { id: '5', name: 'Product Name #6', price: 19.99, image: mickey },
+];
+
+export default products;
