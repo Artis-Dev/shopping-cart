0bec5e2160267d832f3878a2d39c8e33c3e25228
ArtisDev <artisdev@proton.me>
2023-01-27 17:59:43 +0200

    Move router logic to seperate file

--- src/App.js
+++ src/App.js
@@ -1,45 +1,7 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import Cart from './pages/Cart';
-import Contacts from './pages/Contacts';
-import PageNotFound from './pages/PageNotFound';
+import { RouterProvider } from 'react-router-dom';
+import router from './utils/router';
 
 function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        element: <Layout />,
-        children: [
-          {
-            path: '/',
-            element: <Home />,
-          },
-          {
-            path: 'shop',
-            element: <Shop />,
-          },
-          {
-            path: 'cart',
-            element: <Cart />,
-          },
-          {
-            path: 'contacts',
-            element: <Contacts />,
-          },
-          {
-            path: '*',
-            element: <PageNotFound />,
-          },
-        ],
-      },
-    ],
-    {
-      basename: '/shopping-cart',
-    }
-  );
-
   return <RouterProvider router={router} />;
 }
 
---
+++ src/utils/router.js
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from '../components/Layout';
+import Home from '../pages/Home';
+import Shop from '../pages/Shop';
+import Cart from '../pages/Cart';
+import Contacts from '../pages/Contacts';
+import PageNotFound from '../pages/PageNotFound';
+
+const router = createBrowserRouter(
+  [
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: 'shop',
+          element: <Shop />,
+        },
+        {
+          path: 'cart',
+          element: <Cart />,
+        },
+        {
+          path: 'contacts',
+          element: <Contacts />,
+        },
+        {
+          path: '*',
+          element: <PageNotFound />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: '/shopping-cart',
+  }
+);
+
+export default router;
