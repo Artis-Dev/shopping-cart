cda9e58476ece03a14a09807f155cb25085f8f8b
ArtisDev <artisdev@proton.me>
2023-02-01 20:01:03 +0200

    Add button to all products in EmptyCart component

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -1,9 +1,12 @@
+import NavButton from '../Shared/NavButton';
+
 function EmptyCart() {
   return (
     <div className="m-auto flex max-w-screen-xl gap-6 p-12">
       <div className="w-full rounded-lg bg-gray-100 px-12 py-6">
-        <div className="text-center font-semibold">
-          <h2 className="text-2xl">Your cart is empty</h2>
+        <div className="text-center">
+          <h2 className="mb-6 text-2xl font-semibold">Your cart is empty</h2>
+          <NavButton link="/shop" text="Go Shop" />
         </div>
       </div>
     </div>
