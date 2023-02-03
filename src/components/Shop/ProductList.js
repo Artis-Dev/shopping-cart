b8a737765c9ea8ebabb0755bd83061f6cd65942e
ArtisDev <artisdev@proton.me>
2023-02-03 17:26:36 +0200

    Implement ProductList filtering by category and exclusive

--- src/components/Shop/ProductList.js
+++ src/components/Shop/ProductList.js
@@ -1,13 +1,30 @@
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import Product from './Product';
 
 function ProductList() {
-  const { products } = useSelector((state) => state);
+  const { products, filter } = useSelector((state) => state);
+
+  const filteredProducts = useMemo(() => {
+    if (
+      filter.includeCategories.length === 0 &&
+      filter.includeExclusive === false
+    ) {
+      return products;
+    }
+    return products.filter((product) => {
+      const isExclusive = product.exclusive;
+      const isSelectedCategory = filter.includeCategories.includes(
+        product.category
+      );
+      return (filter.includeExclusive && isExclusive) || isSelectedCategory;
+    });
+  }, [filter, products]);
 
   return (
     <div className="grow">
       <div className="grid min-w-[278px] grid-cols-[repeat(auto-fit,minmax(278px,1fr))] gap-6">
-        {products.map((product) => {
+        {filteredProducts.map((product) => {
           return <Product key={product.id} product={product} />;
         })}
       </div>
