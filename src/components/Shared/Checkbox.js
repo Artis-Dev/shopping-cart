f8f8a67ed146f70ea172c334f4c77f765ed01f8c
ArtisDev <artisdev@proton.me>
2023-02-03 17:25:12 +0200

    Implement handleCheckboxChange for filtering products by category and exclusive

--- src/components/Shared/Checkbox.js
+++ src/components/Shared/Checkbox.js
@@ -1,10 +1,18 @@
-import { string } from 'prop-types';
+import { string, func, bool } from 'prop-types';
 
-function Checkbox({ name, id, text }) {
+function Checkbox({ name, id, text, handleChange, checked }) {
   return (
     <div>
       <label className="flex items-center" htmlFor={id}>
-        <input className="mr-1" type="checkbox" name={name} id={id} />
+        <input
+          className="mr-1"
+          type="checkbox"
+          name={name}
+          id={id}
+          value={name}
+          checked={checked}
+          onChange={handleChange}
+        />
         {text}
       </label>
     </div>
@@ -15,6 +23,13 @@ Checkbox.propTypes = {
   name: string.isRequired,
   id: string.isRequired,
   text: string.isRequired,
+  handleChange: func,
+  checked: bool,
+};
+
+Checkbox.defaultProps = {
+  handleChange: null,
+  checked: null,
 };
 
 export default Checkbox;
--- src/components/Shop/Sidebar.js
+++ src/components/Shop/Sidebar.js
@@ -1,20 +1,63 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Checkbox from '../Shared/Checkbox';
 
 function Sidebar() {
+  const dispatch = useDispatch();
+  const { filter } = useSelector((state) => state);
+
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+
+    if (value === 'exclusive') {
+      dispatch({
+        type: 'FILTER_TOGGLE_EXLUSIVE',
+        item: checked,
+      });
+    } else if (checked) {
+      dispatch({ type: 'FILTER_ADD', item: value });
+    } else {
+      dispatch({ type: 'FILTER_REMOVE', item: value });
+    }
+  };
+
   return (
     <div className="w-full text-lg sm:max-w-[278px] ">
       <h2 className="mb-4 text-3xl font-bold">Filters</h2>
       <div className="flex gap-y-4 gap-x-6 sm:flex-col">
         <div>
           <h3 className="mb-2 text-2xl">Exclusivity</h3>
-          <Checkbox name="lorem" id="lorem" text="Lorem" />
+          <Checkbox
+            name="exclusive"
+            id="exclusive"
+            text="Exclusive"
+            checked={filter.includeExclusive}
+            handleChange={handleCheckboxChange}
+          />
         </div>
         <div>
           <h3 className="mb-2 text-2xl">Categories</h3>
           <div className="flex flex-wrap gap-x-4 gap-y-0 sm:flex-col">
-            <Checkbox name="ipsum" id="ipsum" text="Ipsum" />
-            <Checkbox name="dolor" id="dolor" text="Dolor" />
-            <Checkbox name="amet" id="amet" text="Amet" />
+            <Checkbox
+              name="ipsum"
+              id="ipsum"
+              text="Ipsum"
+              checked={filter.includeCategories.includes('ipsum')}
+              handleChange={handleCheckboxChange}
+            />
+            <Checkbox
+              name="dolor"
+              id="dolor"
+              text="Dolor"
+              checked={filter.includeCategories.includes('dolor')}
+              handleChange={handleCheckboxChange}
+            />
+            <Checkbox
+              name="amet"
+              id="amet"
+              text="Amet"
+              checked={filter.includeCategories.includes('amet')}
+              handleChange={handleCheckboxChange}
+            />
           </div>
         </div>
       </div>
