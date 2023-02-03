7fd198f43674bf4f29c8eb76b728c6c3ba1b9d9c
ArtisDev <artisdev@proton.me>
2023-02-03 17:16:11 +0200

    Add 'category' and 'exclusive' properties to product object

--- src/utils/products.js
+++ src/utils/products.js
@@ -2,12 +2,53 @@ import uniqid from 'uniqid';
 import mickey from '../assets/mickey.webp';
 
 const products = [
-  { id: uniqid(), name: 'Product Name #1', price: 10.0, image: mickey },
-  { id: uniqid(), name: 'Product Name #2', price: 14.99, image: mickey },
-  { id: uniqid(), name: 'Product Name #3', price: 9.5, image: mickey },
-  { id: uniqid(), name: 'Product Name #4', price: 7.7, image: mickey },
-  { id: uniqid(), name: 'Product Name #5', price: 12.0, image: mickey },
-  // { id: '5', name: 'Product Name #6', price: 19.99, image: mickey },
+  {
+    id: uniqid(),
+    name: 'Product Name #1 (dolor)',
+    price: 10.0,
+    image: mickey,
+    category: 'dolor',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Product Name #2 (dolor)',
+    price: 14.99,
+    image: mickey,
+    category: 'dolor',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Product Name #3 (ipsum)',
+    price: 9.5,
+    image: mickey,
+    category: 'ipsum',
+    exclusive: true,
+  },
+  {
+    id: uniqid(),
+    name: 'Product Name #4 (amet)',
+    price: 7.7,
+    image: mickey,
+    category: 'amet',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Product Name #5 (amet)',
+    price: 12.0,
+    image: mickey,
+    category: 'amet',
+    exclusive: true,
+  },
+  // {
+  //   id: uniqid(),
+  //   name: 'Product Name #6 (amet)',
+  //   price: 19.99,
+  //   image: mickey,
+  //   category: 'amet',
+  // },
 ];
 
 export default products;
