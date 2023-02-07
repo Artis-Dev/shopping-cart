2daea4fb8e59ac2c9fed1bc4a87f934eb3ab5f5f
ArtisDev <artisdev@proton.me>
2023-02-07 18:11:28 +0200

    Add basic 404 layout

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,19 @@
+import NavButton from '../components/Shared/NavButton';
+
 function PageNotFound() {
-  return <h1>404</h1>;
+  return (
+    <div className="m-auto flex max-w-screen-xl gap-6 p-12">
+      <div className="w-full rounded-lg bg-gray-100 px-12 py-6">
+        <div className="text-center">
+          <h2 className="mb-6 text-6xl font-semibold">404</h2>
+          <h2 className="mb-6 text-2xl font-semibold">
+            Sorry. We couldn&apos;t find this page.
+          </h2>
+          <NavButton link="/" text="Homepage" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;
