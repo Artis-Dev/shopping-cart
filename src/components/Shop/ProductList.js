2536c86e8cdf92e1328d814732a89c19fa16f8b7
ArtisDev <artisdev@proton.me>
2023-02-14 12:56:58 +0200

    Move 'getFilteredProducts' function call to Shop.js

--- src/components/Shop/ProductList.js
+++ src/components/Shop/ProductList.js
@@ -1,17 +1,8 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import useFilterActions from '../../utils/useFilterActions';
 import Product from './Product';
 
 function ProductList() {
-  const { getFilteredProducts } = useFilterActions();
-  const { category } = useParams();
   const { filter } = useSelector((state) => state);
-
-  useEffect(() => {
-    getFilteredProducts(category);
-  }, [getFilteredProducts, category]);
 
   return (
     <div className="grid min-w-[278px] grid-cols-[repeat(auto-fit,minmax(278px,1fr))] gap-6">
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,14 +1,20 @@
 import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import useFilterActions from '../utils/useFilterActions';
 import ProductsSection from '../components/Shop/ProductsSection';
 import Sidebar from '../components/Shop/Sidebar';
 
 function Shop() {
-  const { processQueryParams } = useFilterActions();
+  const { processQueryParams, getFilteredProducts } = useFilterActions();
+  const { category } = useParams();
 
   useEffect(() => {
     processQueryParams();
   }, [processQueryParams]);
+
+  useEffect(() => {
+    getFilteredProducts(category);
+  }, [getFilteredProducts, category]);
 
   return (
     <div className="m-auto flex max-w-screen-xl flex-col gap-6 p-12 sm:flex-row">
