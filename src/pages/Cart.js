13ce4b29d20617b81471dfad748fce0e56804578
ArtisDev <artisdev@proton.me>
2023-02-14 13:13:53 +0200

    Remove redundant prop

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -8,7 +8,7 @@ function Cart() {
   return (
     <div className="m-auto flex max-w-screen-xl gap-6 p-12">
       <div className="w-full rounded-lg bg-gray-100 p-6">
-        {cart.length === 0 ? <EmptyCart /> : <CartList cart={cart} />}
+        {cart.length === 0 ? <EmptyCart /> : <CartList />}
       </div>
     </div>
   );
