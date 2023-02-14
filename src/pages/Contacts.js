06c5a313a685b41c5008244dc65fe6b9158f6e95
ArtisDev <artisdev@proton.me>
2023-02-14 12:59:42 +0200

    Update Contacts.js

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,5 +1,13 @@
 function Contacts() {
-  return <h1>Contacts</h1>;
+  return (
+    <div className="m-auto flex max-w-screen-xl gap-6 p-12">
+      <div className="w-full rounded-lg bg-gray-100 px-12 py-6">
+        <div className="text-center">
+          <h2 className="text-2xl font-semibold">Contacts</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contacts;
