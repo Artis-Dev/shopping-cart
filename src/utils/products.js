6b075745776abeed2de05ea172e42f0d54b4d20a
ArtisDev <artisdev@proton.me>
2023-02-17 14:25:02 +0200

    Update products.js

--- src/utils/products.js
+++ src/utils/products.js
@@ -1,55 +1,330 @@
 import uniqid from 'uniqid';
-import mickey from '../assets/mickey.webp';
+import img46627 from '../assets/products/46627.webp';
+import img48063 from '../assets/products/48063.webp';
+import img48891 from '../assets/products/48891.webp';
+import img54296 from '../assets/products/54296.webp';
+import img54463 from '../assets/products/54463.webp';
+import img55922 from '../assets/products/55922.webp';
+import img57641 from '../assets/products/57641.webp';
+import img57643 from '../assets/products/57643.webp';
+import img57813 from '../assets/products/57813.webp';
+import img57814 from '../assets/products/57814.webp';
+import img57815 from '../assets/products/57815.webp';
+import img58846 from '../assets/products/58846.webp';
+import img59254 from '../assets/products/59254.webp';
+import img59331 from '../assets/products/59331.webp';
+import img59333 from '../assets/products/59333.webp';
+import img59621 from '../assets/products/59621.webp';
+import img59623 from '../assets/products/59623.webp';
+import img60279 from '../assets/products/60279.webp';
+import img60462 from '../assets/products/60462.webp';
+import img61416 from '../assets/products/61416.webp';
+import img61417 from '../assets/products/61417.webp';
+import img63984 from '../assets/products/63984.webp';
+import img64231 from '../assets/products/64231.webp';
+import img64636 from '../assets/products/64636.webp';
+import img65041 from '../assets/products/65041.webp';
+import img65042 from '../assets/products/65042.webp';
+import img65043 from '../assets/products/65043.webp';
+import img65419 from '../assets/products/65419.webp';
+import img65422 from '../assets/products/65422.webp';
+import img65641 from '../assets/products/65641.webp';
+import img65643 from '../assets/products/65643.webp';
+import img65650 from '../assets/products/65650.webp';
+import img65653 from '../assets/products/65653.webp';
+import img67606 from '../assets/products/67606.webp';
+import img68620 from '../assets/products/68620.webp';
+import img68854 from '../assets/products/68854.webp';
 
 const products = [
   {
     id: uniqid(),
-    name: 'Product Name #1 (dolor)',
-    price: 10.0,
-    image: mickey,
-    category: 'dolor',
+    name: 'Trinity - Matrix (2021)',
+    price: 16.0,
+    image: img59254,
+    category: 'movies',
     exclusive: false,
   },
   {
     id: uniqid(),
-    name: 'Product Name #2 (dolor)',
-    price: 14.99,
-    image: mickey,
-    category: 'dolor',
-    exclusive: false,
-  },
-  {
-    id: uniqid(),
-    name: 'Product Name #3 (ipsum)',
-    price: 9.5,
-    image: mickey,
-    category: 'ipsum',
+    name: 'Morpheus (Pink Suit) - Matrix (2021)',
+    price: 18.0,
+    image: img60279,
+    category: 'movies',
     exclusive: true,
   },
   {
     id: uniqid(),
-    name: 'Product Name #4 (amet)',
-    price: 7.7,
-    image: mickey,
-    category: 'amet',
+    name: 'Batman (Battle Damaged) - The Batman',
+    price: 18.0,
+    image: img60462,
+    category: 'movies',
+    exclusive: true,
+  },
+  {
+    id: uniqid(),
+    name: 'Scorpion (Action Pose) (Metallic) - Mortal Kombat',
+    price: 18.0,
+    image: img54296,
+    category: 'movies',
+    exclusive: true,
+  },
+  {
+    id: uniqid(),
+    name: 'Spider-Man - Spider-Man: No Way Home',
+    price: 16.0,
+    image: img67606,
+    category: 'movies',
     exclusive: false,
   },
   {
     id: uniqid(),
-    name: 'Product Name #5 (amet)',
-    price: 12.0,
-    image: mickey,
-    category: 'amet',
+    name: 'Carnage (Carla Unger) - Venom',
+    price: 18.0,
+    image: img48891,
+    category: 'movies',
     exclusive: true,
-    new: true,
   },
-  // {
-  //   id: uniqid(),
-  //   name: 'Product Name #6 (amet)',
-  //   price: 19.99,
-  //   image: mickey,
-  //   category: 'amet',
-  // },
+  {
+    id: uniqid(),
+    name: 'Harry Potter With Invisibility Cloak - Harry Potter',
+    price: 16.0,
+    image: img48063,
+    category: 'movies',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Hermione (Chamber of Secrets) - Harry Potter',
+    price: 16.0,
+    image: img65653,
+    category: 'movies',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Dobby (Chamber of Secrets) - Harry Potter',
+    price: 16.0,
+    image: img65650,
+    category: 'movies',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Jake Sully - Avatar',
+    price: 16.0,
+    image: img65641,
+    category: 'movies',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Battle Neytiri - Avatar',
+    price: 16.0,
+    image: img65643,
+    category: 'movies',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Hannibal - The Silence of the Lambs',
+    price: 16.0,
+    image: img63984,
+    category: 'movies',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Donald Duck - Disney Mickey and Friends',
+    price: 16.0,
+    image: img59621,
+    category: 'television',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Minnie Mouse - Disney Mickey and Friends',
+    price: 16.0,
+    image: img59623,
+    category: 'television',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Minnie Mouse (Facet) - Disney 100th',
+    price: 18.0,
+    image: img68620,
+    category: 'television',
+    exclusive: true,
+  },
+  {
+    id: uniqid(),
+    name: 'Ciri - The Witcher (Netflix)',
+    price: 16.0,
+    image: img57813,
+    category: 'television',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Geralt - The Witcher (Netflix)',
+    price: 16.0,
+    image: img57814,
+    category: 'television',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Yennefer - The Witcher (Netflix)',
+    price: 16.0,
+    image: img57815,
+    category: 'television',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Jaskier (Green Outfit) - The Witcher (Netflix)',
+    price: 18.0,
+    image: img58846,
+    category: 'television',
+    exclusive: true,
+  },
+  {
+    id: uniqid(),
+    name: 'Haise Sasaki - Tokyo Ghoul:re',
+    price: 16.0,
+    image: img57641,
+    category: 'television',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Saiko Yonebayashi - Tokyo Ghoul:re',
+    price: 16.0,
+    image: img57643,
+    category: 'television',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Bonekichi - One Piece',
+    price: 16.0,
+    image: img54463,
+    category: 'television',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Gaara - Naruto',
+    price: 16.0,
+    image: img46627,
+    category: 'television',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Tsunade (Creation Rebirth) - Naruto Shippuden',
+    price: 18.0,
+    image: img68854,
+    category: 'television',
+    exclusive: true,
+  },
+  {
+    id: uniqid(),
+    name: "Tiny Tina - Tiny Tina's Wonderlands",
+    price: 16.0,
+    image: img59331,
+    category: 'games',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: "Dragon Lord - Tiny Tina's Wonderlands",
+    price: 16.0,
+    image: img59333,
+    category: 'games',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: "Devil's Throne - Cuphead",
+    price: 35.0,
+    image: img61416,
+    category: 'games',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Aeroplane Ms. Chalice - Cuphead',
+    price: 16.0,
+    image: img61417,
+    category: 'games',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Aether - Genshin Impact',
+    price: 16.0,
+    image: img65419,
+    category: 'games',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Paimon - Genshin Impact',
+    price: 16.0,
+    image: img65422,
+    category: 'games',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Squirtle (Diamond Collection) - Pokémon',
+    price: 22.0,
+    image: img55922,
+    category: 'games',
+    exclusive: true,
+  },
+  {
+    id: uniqid(),
+    name: 'Eevee (Flocked) - Pokémon',
+    price: 18.0,
+    image: img64636,
+    category: 'games',
+    exclusive: true,
+  },
+  {
+    id: uniqid(),
+    name: 'Cubone - Pokémon',
+    price: 16.0,
+    image: img65041,
+    category: 'games',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Flareon - Pokémon',
+    price: 16.0,
+    image: img65042,
+    category: 'games',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: 'Pikachu (Grumpy) - Pokémon',
+    price: 16.0,
+    image: img65043,
+    category: 'games',
+    exclusive: false,
+  },
+  {
+    id: uniqid(),
+    name: "Tie-Dye Foxy - Five Nights at Freddy's",
+    price: 16.0,
+    image: img64231,
+    category: 'games',
+    exclusive: false,
+  },
 ];
 
 export default products;
