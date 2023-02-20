2a36438c95000e090378f7892550ddfb24ec07af
ArtisDev <artisdev@proton.me>
2023-02-20 18:45:10 +0200

    Remove active style from shop link if there is invalid category in url

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,11 +1,17 @@
-import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { NavLink, useParams } from 'react-router-dom';
 import useFilterActions from '../../utils/useFilterActions';
 
 function Navigation() {
+  const { categories } = useSelector((state) => state.filter);
+  const { category } = useParams();
   const { handleReset } = useFilterActions();
+
   const activeStyle = {
     textDecoration: 'underline',
   };
+
+  const isValidCategory = categories.includes(category);
 
   return (
     <nav className="flex flex-col gap-6 sm:grow sm:flex-row sm:justify-center">
@@ -20,7 +26,9 @@ function Navigation() {
         to="/shop"
         onClick={handleReset}
         className="link"
-        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        style={({ isActive }) =>
+          isActive && (!category || isValidCategory) ? activeStyle : undefined
+        }
       >
         Shop
       </NavLink>
