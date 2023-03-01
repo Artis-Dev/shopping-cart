53770f3a1959ebd3dc99ae99fae854d8024f0623
ArtisDev <artisdev@proton.me>
2023-03-01 14:08:23 +0200

    Add onClick handler with handleReset for homepage category links

--- src/components/Home/CategoryBox.js
+++ src/components/Home/CategoryBox.js
@@ -1,11 +1,15 @@
 import { string } from 'prop-types';
 import { Link } from 'react-router-dom';
+import useFilterActions from '../../utils/useFilterActions';
 
 function CategoryBox({ text, category }) {
+  const { handleReset } = useFilterActions();
+
   return (
     <Link
       to={`/shop/${category}`}
       className="flex h-60 w-full grow rounded-lg bg-gray-200 p-6"
+      onClick={handleReset}
     >
       <div className="text-3xl">{text}</div>
     </Link>
