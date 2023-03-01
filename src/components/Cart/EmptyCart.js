a7ba28f500bf8e8a98fa1a7606ef370b6895c9c9
ArtisDev <artisdev@proton.me>
2023-03-01 16:36:19 +0200

    Change Empty Cart texts

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -4,8 +4,13 @@ function EmptyCart() {
   return (
     <div className="w-full rounded-lg bg-gray-100">
       <div className="text-center">
-        <h2 className="mb-6 text-2xl font-semibold">Your cart is empty</h2>
-        <NavButton link="/shop" text="Go Shop" />
+        <h2 className="mb-6 text-2xl font-semibold">
+          Your cart is currently empty.
+        </h2>
+        <h2 className="mb-6 text-xl font-semibold">
+          Let&apos;s find your next favorite character to add to the collection!
+        </h2>
+        <NavButton link="/" text="Go shop" />
       </div>
     </div>
   );
