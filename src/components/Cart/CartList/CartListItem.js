347341cc13cebc986dbdf09b91b194ac95f1a5a9
ArtisDev <artisdev@proton.me>
2023-03-02 20:36:45 +0200

    Added Link component for image and title redirect in cart

--- src/components/Cart/CartList/CartListItem.js
+++ src/components/Cart/CartList/CartListItem.js
@@ -1,4 +1,5 @@
 import { shape, string, number } from 'prop-types';
+import { Link } from 'react-router-dom';
 import prettyPrice from '../../../utils/prettyPrice';
 import ProductCounter from '../../Shared/ProductCounter';
 import CartListItemRemove from './CartListItemRemove';
@@ -7,11 +8,16 @@ function CartListItem({ product }) {
   return (
     <div className="mb-3 flex gap-2 border-b pb-3 last:mb-0 last:border-0 last:pb-0">
       <div className="h-32 w-24 shrink-0 sm:w-32">
-        <img className="-ml-4 w-32 max-w-none" src={product.image} alt="" />
+        <Link to={`/product/${product.id}`}>
+          <img className="-ml-4 w-32 max-w-none" src={product.image} alt="" />
+        </Link>
       </div>
       <div className="flex grow flex-col gap-2 md:flex-row">
-        <div className="flex h-auto grow items-center overflow-hidden text-xl">
-          {product.name}
+        <div
+          to={`/product/${product.id}`}
+          className="flex h-auto grow items-center overflow-hidden text-xl"
+        >
+          <Link to={`/product/${product.id}`}>{product.name}</Link>
         </div>
         <div className="flex h-auto shrink-0 items-center justify-between gap-2 md:justify-center">
           <div className="flex w-[89px] flex-col items-center gap-2">
