a9c09bfd2e03d09929ac882a1063bc70b5cbe915
ArtisDev <artisdev@proton.me>
2024-01-15 12:23:14 +0200

    Fix empty cart layout

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,23 +7,27 @@ import CartSummary from '../components/Cart/CartSummary';
 function Cart() {
   const { cart } = useSelector((state) => state);
 
-  if (cart.length === 0) {
-    return <EmptyCart />;
-  }
-
   return (
     <div className="m-auto flex max-w-screen-xl flex-col gap-6 p-6 sm:p-12">
-      <div className="w-full rounded-lg bg-gray-100 p-6">
-        <CartList />
-      </div>
-      <div className="flex gap-6">
-        <div className="w-1/2 rounded-lg bg-gray-100 p-6">
-          <CartShipping />
+      {cart.length === 0 ? (
+        <div className="w-full rounded-lg bg-gray-100 p-6">
+          <EmptyCart />
         </div>
-        <div className="w-1/2 rounded-lg bg-gray-100 p-6">
-          <CartSummary />
-        </div>
-      </div>
+      ) : (
+        <>
+          <div className="w-full rounded-lg bg-gray-100 p-6">
+            <CartList />
+          </div>
+          <div className="flex gap-6">
+            <div className="w-1/2 rounded-lg bg-gray-100 p-6">
+              <CartShipping />
+            </div>
+            <div className="w-1/2 rounded-lg bg-gray-100 p-6">
+              <CartSummary />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
